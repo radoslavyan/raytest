8e0259f50e3c9357254d1138578cb58b26e7ae83
radoslavyan <radoslav.yanakiev@hotmail.com>
2023-11-13 09:25:14 +0100

    fourth commit

---
+++ DotNetApiSource/RayWebApi/RayWebApi/Startup.cs
@@ -0,0 +1,46 @@
+﻿namespace RayWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                
+            }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            
+          
+        }
+
+    }
+}
